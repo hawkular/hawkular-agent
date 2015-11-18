138af158e94170e5da825cbe5782e8e043086668
Juraci Paixão Kröhling <juraci@kroehling.de>
2015-11-18 09:26:45 +0100

    HAWKULAR-812 - Use AES/CBC/PKCS5PADDING for agent encryption with a PBKDF2 key.

--- hawkular-wildfly-agent-installer/src/main/java/org/hawkular/wildfly/agent/installer/EncoderDecoder.java
+++ hawkular-wildfly-agent-installer/src/main/java/org/hawkular/wildfly/agent/installer/EncoderDecoder.java
@@ -16,7 +16,15 @@
  */
 package org.hawkular.wildfly.agent.installer;
 
+import java.security.InvalidKeyException;
+import java.security.spec.KeySpec;
 import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Provides a simple encoder/decoder. It requires an encryption key to encode and then later decode a string.
@@ -31,58 +39,99 @@ public final class EncoderDecoder {
 
     /**
      * Given a secret key, this will encode the given clear text message and return the encoded string.
-     * In order to decode the string, pass the returned string with the same key to {@link #decode(String, String)}.
+     * In order to decode the string, pass the returned string with the same key and salt to
+     * {@link #decode(String, String, byte[])}.
      *
-     * @param key the secret key used to encrypt the clear text string (must not be null)
      * @param clearText the message to encrypt (if null, then null is returned)
+     * @param key the secret key used to encrypt the clear text string (must not be null)
+     * @param salt random sequence to be used with the key to encrypt the clear text string (must not be null)
      * @return the encrypted message
+     * @throws IllegalArgumentException if the key and/or salt is null or if the salt is of a wrong size
      * @throws Exception on error
      */
-    public static String encode(String key, String clearText) throws Exception {
-        if (clearText == null) {
+    public static String encode(String clearText, String key, byte[] salt) throws Exception {
+        if (null == clearText) {
             return null;
         }
 
-        if (key == null || key.isEmpty()) {
-            throw new IllegalArgumentException("You must provide a key to encrypt the string");
+        if (null == key || null == salt) {
+            throw new IllegalArgumentException("Key and salt must be specified.");
         }
 
-        StringBuilder encodedString = new StringBuilder();
-        for (int i = 0; i < clearText.length(); i++) {
-            char keyChar = key.charAt(i % key.length());
-            char encChar = (char) ((clearText.charAt(i) + keyChar) % ((char) 256));
-            encodedString.append(encChar);
+        SecretKeySpec keySpec = getKeySpecForKeyAndSalt(key, salt);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        } catch (InvalidKeyException invalidKeyException) {
+            // user needs the unlimited jurisdiction files...
+            keySpec = getWeakKeySpecForKeyAndSalt(key, salt);
+            cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
         }
-        return new String(Base64.getEncoder().encode(encodedString.toString().getBytes()), "UTF-8");
+
+        byte[] encryptedData = cipher.doFinal(clearText.getBytes("UTF-8"));
+        byte[] iv = cipher.getIV();
+
+        if (null != iv) {
+            String ivAsString = new String(Base64.getEncoder().encode(cipher.getIV()), "UTF-8");
+            String encryptedAsString = new String(Base64.getEncoder().encode(encryptedData), "UTF-8");
+            return ivAsString + "$" + encryptedAsString;
+        } else {
+            return new String(Base64.getEncoder().encode(encryptedData), "UTF-8");
+        }
     }
 
     /**
      * Decrypts the given encoded string using the given secret key. The encoded string must have
-     * been encoded via the {@link #encode(String, String)} method with the same key in order
+     * been encoded via the {@link #encode(String, String, byte[])} method with the same key in order
      * for the decode to work.
      *
+     * @param encodedString the encoded message (if null, then null is returned)
      * @param key the key that was used to encode the string (must not be null)
-     * @param encodedString the encoded mesage (if null, then null is returned)
+     * @param salt random sequence that was used to with the key to encode the string (must not be null)
      * @return the original, clear text message that was encoded
+     * @throws IllegalArgumentException if the key and/or salt is null or if the salt is of a wrong size
      * @throws Exception on error
      */
-    public static String decode(String key, String encodedString) throws Exception {
-        if (encodedString == null) {
+    public static String decode(String encodedString, String key, byte[] salt) throws Exception {
+        if (null == encodedString) {
             return null;
         }
 
-        if (key == null || key.isEmpty()) {
-            throw new IllegalArgumentException("You must provide a key to decrypt the string");
+        if (null == key || null == salt) {
+            throw new IllegalArgumentException("Key and salt must be specified.");
         }
 
-        StringBuilder clearText = new StringBuilder();
-        encodedString = new String(Base64.getDecoder().decode(encodedString), "UTF-8");
-        for (int i = 0; i < encodedString.length(); i++) {
-            int keyChar = key.charAt(i % key.length());
-            char decChar = (char) ((encodedString.charAt(i) - keyChar) % ((char) 256));
-            clearText.append(decChar);
+        SecretKeySpec keySpec;
+        Cipher cipher;
+        byte[] encodedBytes;
+        if (encodedString.contains("$")) {
+            keySpec = getKeySpecForKeyAndSalt(key, salt);
+            cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            String[] parts = encodedString.split("\\$");
+            byte[] iv = Base64.getDecoder().decode(parts[0]);
+            encodedBytes = Base64.getDecoder().decode(parts[1]);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv));
+        } else {
+            keySpec = getWeakKeySpecForKeyAndSalt(key, salt);
+            cipher = Cipher.getInstance("DES");
+            encodedBytes = Base64.getDecoder().decode(encodedString);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);
         }
-        return clearText.toString();
+
+        return new String(cipher.doFinal(encodedBytes), "UTF-8");
     }
 
+    private static SecretKeySpec getKeySpecForKeyAndSalt(String key, byte[] salt) throws Exception {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(key.toCharArray(), salt, 80_000, 256);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    }
+
+    private static SecretKeySpec getWeakKeySpecForKeyAndSalt(String key, byte[] salt) throws Exception {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(key.toCharArray(), salt, 80_000, 64);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "DES");
+    }
 }
