1d544b3b7dae39d6a42c956b05f0126bb2532906
John Mazzitelli <mazz@redhat.com>
2017-04-26 13:34:04 -0400

    hwkagent-169 support expressions in security realm

--- hawkular-javaagent/src/main/java/org/hawkular/agent/javaagent/config/SecurityRealm.java
+++ hawkular-javaagent/src/main/java/org/hawkular/agent/javaagent/config/SecurityRealm.java
@@ -48,16 +48,16 @@ public class SecurityRealm implements Validatable {
     private StringExpression keyPassword;
 
     @JsonProperty(value = "keystore-type")
-    private String keystoreType = KeyStore.getDefaultType();
+    private StringExpression keystoreType = new StringExpression(KeyStore.getDefaultType());
 
     @JsonProperty(value = "key-manager-algorithm")
-    private String keyManagerAlgorithm = KeyManagerFactory.getDefaultAlgorithm();
+    private StringExpression keyManagerAlgorithm = new StringExpression(KeyManagerFactory.getDefaultAlgorithm());
 
     @JsonProperty(value = "trust-manager-algorithm")
-    private String trustManagerAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+    private StringExpression trustManagerAlgorithm = new StringExpression(TrustManagerFactory.getDefaultAlgorithm());
 
     @JsonProperty(value = "ssl-protocol")
-    private String sslProtocol = "TLSv1";
+    private StringExpression sslProtocol = new StringExpression("TLSv1");
 
     public SecurityRealm() {
     }
@@ -68,10 +68,12 @@ public class SecurityRealm implements Validatable {
         this.keystorePassword = original.keystorePassword == null ? null
                 : new StringExpression(original.keystorePassword);
         this.keyPassword = original.keyPassword == null ? null : new StringExpression(original.keyPassword);
-        this.keystoreType = original.keystoreType;
-        this.keyManagerAlgorithm = original.keyManagerAlgorithm;
-        this.trustManagerAlgorithm = original.trustManagerAlgorithm;
-        this.sslProtocol = original.sslProtocol;
+        this.keystoreType = original.keystoreType == null ? null : new StringExpression(original.keystoreType);
+        this.keyManagerAlgorithm = original.keyManagerAlgorithm == null ? null
+                : new StringExpression(original.keyManagerAlgorithm);
+        this.trustManagerAlgorithm = original.trustManagerAlgorithm == null ? null
+                : new StringExpression(original.trustManagerAlgorithm);
+        this.sslProtocol = original.sslProtocol == null ? null : new StringExpression(original.sslProtocol);
     }
 
     @Override
@@ -88,34 +90,48 @@ public class SecurityRealm implements Validatable {
             throw new Exception("security-realm: [" + name + "] keystore-password must be specified");
         }
 
+        if (keystoreType == null) {
+            throw new Exception("security-realm: [" + name + "] keystore-type must be specified");
+        }
         try {
-            KeyStore.getInstance(keystoreType);
+            KeyStore.getInstance(keystoreType.get().toString());
         } catch (Exception e) {
-            throw new Exception("security-realm: [" + name + "] keystore-type [" + keystoreType
+            throw new Exception("security-realm: [" + name + "] keystore-type [" + keystoreType.get()
                     + "] is invalid. You may want to use the VM default of ["
                     + KeyStore.getDefaultType() + "]", e);
         }
 
+        if (keyManagerAlgorithm == null) {
+            throw new Exception("security-realm: [" + name + "] key-manager-algorithm must be specified");
+        }
         try {
-            KeyManagerFactory.getInstance(keyManagerAlgorithm);
+            KeyManagerFactory.getInstance(keyManagerAlgorithm.get().toString());
         } catch (Exception e) {
-            throw new Exception("security-realm: [" + name + "] key-manager-algorithm [" + keyManagerAlgorithm
+            throw new Exception("security-realm: [" + name + "] key-manager-algorithm [" + keyManagerAlgorithm.get()
                     + "] is invalid. You may want to use the VM default of ["
                     + KeyManagerFactory.getDefaultAlgorithm() + "]", e);
         }
 
+        if (trustManagerAlgorithm == null) {
+            throw new Exception("security-realm: [" + name + "] trust-manager-algorithm must be specified");
+        }
         try {
-            TrustManagerFactory.getInstance(trustManagerAlgorithm);
+            TrustManagerFactory.getInstance(trustManagerAlgorithm.get().toString());
         } catch (Exception e) {
-            throw new Exception("security-realm: [" + name + "] trust-manager-algorithm [" + trustManagerAlgorithm
-                    + "] is invalid. You may want to use the VM default of ["
-                    + TrustManagerFactory.getDefaultAlgorithm() + "]", e);
+            throw new Exception(
+                    "security-realm: [" + name + "] trust-manager-algorithm [" + trustManagerAlgorithm.get()
+                            + "] is invalid. You may want to use the VM default of ["
+                            + TrustManagerFactory.getDefaultAlgorithm() + "]",
+                    e);
         }
 
+        if (sslProtocol == null) {
+            throw new Exception("security-realm: [" + name + "] ssl-protocol must be specified");
+        }
         try {
-            SSLContext.getInstance(sslProtocol);
+            SSLContext.getInstance(sslProtocol.get().toString());
         } catch (Exception e) {
-            throw new Exception("security-realm: [" + name + "] ssl-protocol [" + sslProtocol
+            throw new Exception("security-realm: [" + name + "] ssl-protocol [" + sslProtocol.get()
                     + "] is invalid. You may want to use [TLSv1]", e);
         }
     }
@@ -165,34 +181,50 @@ public class SecurityRealm implements Validatable {
     }
 
     public String getKeystoreType() {
-        return keystoreType;
+        return keystoreType == null ? null : keystoreType.get().toString();
     }
 
     public void setKeystoreType(String keystoreType) {
-        this.keystoreType = keystoreType;
+        if (this.keystoreType != null) {
+            this.keystoreType.set(new StringValue(keystoreType));
+        } else {
+            this.keystoreType = new StringExpression(new StringValue(keystoreType));
+        }
     }
 
     public String getKeyManagerAlgorithm() {
-        return keyManagerAlgorithm;
+        return keyManagerAlgorithm == null ? null : keyManagerAlgorithm.get().toString();
     }
 
     public void setKeyManagerAlgorithm(String keyManagerAlgorithm) {
-        this.keyManagerAlgorithm = keyManagerAlgorithm;
+        if (this.keyManagerAlgorithm != null) {
+            this.keyManagerAlgorithm.set(new StringValue(keyManagerAlgorithm));
+        } else {
+            this.keyManagerAlgorithm = new StringExpression(new StringValue(keyManagerAlgorithm));
+        }
     }
 
     public String getTrustManagerAlgorithm() {
-        return trustManagerAlgorithm;
+        return trustManagerAlgorithm == null ? null : trustManagerAlgorithm.get().toString();
     }
 
     public void setTrustManagerAlgorithm(String trustManagerAlgorithm) {
-        this.trustManagerAlgorithm = trustManagerAlgorithm;
+        if (this.trustManagerAlgorithm != null) {
+            this.trustManagerAlgorithm.set(new StringValue(trustManagerAlgorithm));
+        } else {
+            this.trustManagerAlgorithm = new StringExpression(new StringValue(trustManagerAlgorithm));
+        }
     }
 
     public String getSslProtocol() {
-        return sslProtocol;
+        return sslProtocol == null ? null : sslProtocol.get().toString();
     }
 
     public void setSslProtocol(String sslProtocol) {
-        this.sslProtocol = sslProtocol;
+        if (this.sslProtocol != null) {
+            this.sslProtocol.set(new StringValue(sslProtocol));
+        } else {
+            this.sslProtocol = new StringExpression(new StringValue(sslProtocol));
+        }
     }
 }
