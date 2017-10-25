bdf94d10eab170ced013d765601f13a4d9530661
John Mazzitelli <mazz@redhat.com>
2017-10-25 15:26:54 -0400

    fork prometheus jmx exporter

---
+++ hawkular-agent-core/src/main/java/io/prometheus/jmx/WebServer.java
@@ -0,0 +1,32 @@
+package io.prometheus.jmx;
+
+import java.io.File;
+import java.net.InetSocketAddress;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exporter.HTTPServer;
+
+public class WebServer {
+
+   public static void main(String[] args) throws Exception {
+     if (args.length < 2) {
+       System.err.println("Usage: WebServer <[hostname:]port> <yaml configuration file>");
+       System.exit(1);
+     }
+
+     String[] hostnamePort = args[0].split(":");
+     int port;
+     InetSocketAddress socket;
+     
+     if (hostnamePort.length == 2) {
+       port = Integer.parseInt(hostnamePort[1]);
+       socket = new InetSocketAddress(hostnamePort[0], port);
+     } else {
+       port = Integer.parseInt(hostnamePort[0]);
+       socket = new InetSocketAddress(port);
+     }
+
+     new JmxCollector(new File(args[1])).register();
+     new HTTPServer(socket, CollectorRegistry.defaultRegistry);
+   }
+}
