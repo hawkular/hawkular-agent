7d7d8dd7b80ad476bc3fb9b061a894ea98ab58b6
John Mazzitelli <mazz@redhat.com>
2017-10-30 19:11:30 -0400

    create yaml for domain during build time, not during runtime (#396)
    * create yaml for domain during build time, not during runtime after each time server is started.
    
    * make the env var names consistent with those used in h-services
    
    * rename HAWKULAR_MODE to HAWKULAR_AGENT_MODE since it really involves the agent and the server it is running in.

--- docker-dist/src/main/resources/run_hawkular_javaagent.sh
+++ docker-dist/src/main/resources/run_hawkular_javaagent.sh
@@ -29,16 +29,16 @@ import_hawkular_services_public_key() {
 }
 
 run_hawkular_agent() {
-    ${JBOSS_HOME}/bin/${HAWKULAR_MODE}.sh -b 0.0.0.0
+    ${JBOSS_HOME}/bin/${HAWKULAR_AGENT_MODE}.sh -b 0.0.0.0
 }
 
 main() {
-  HAWKULAR_MODE=`echo ${HAWKULAR_MODE} | tr '[:upper:]' '[:lower:]'`
-  if [[ "${HAWKULAR_MODE}" != "standalone" ]] && [[ "${HAWKULAR_MODE}" != "domain" ]]; then
-    echo 'HAWKULAR_MODE must be set to "standalone" or "domain", found:' ${HAWKULAR_MODE}
+  HAWKULAR_AGENT_MODE=`echo ${HAWKULAR_AGENT_MODE} | tr '[:upper:]' '[:lower:]'`
+  if [[ "${HAWKULAR_AGENT_MODE}" != "standalone" ]] && [[ "${HAWKULAR_AGENT_MODE}" != "domain" ]]; then
+    echo 'HAWKULAR_AGENT_MODE must be set to "standalone" or "domain", found:' ${HAWKULAR_AGENT_MODE}
     exit
   fi
-  if [[ "${HAWKULAR_MODE}" == "domain" ]]; then
+  if [[ "${HAWKULAR_AGENT_MODE}" == "domain" ]]; then
     sed -i "s|- Standalone Environment|- Domain Environment|g" /opt/hawkular/configuration/hawkular-javaagent-config.yaml
   fi
   import_hawkular_services_public_key
