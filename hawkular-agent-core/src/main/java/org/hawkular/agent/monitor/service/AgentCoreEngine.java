b0bb2ad431e930ae9888a11b65e61767cded1cc6
John Mazzitelli <mazz@redhat.com>
2017-10-31 13:31:00 -0400

    register metrics endpoint

--- hawkular-agent-core/src/main/java/org/hawkular/agent/monitor/service/AgentCoreEngine.java
+++ hawkular-agent-core/src/main/java/org/hawkular/agent/monitor/service/AgentCoreEngine.java
@@ -32,6 +32,7 @@ import org.hawkular.agent.monitor.cmd.FeedCommProcessor;
 import org.hawkular.agent.monitor.cmd.WebSocketClientBuilder;
 import org.hawkular.agent.monitor.config.AgentCoreEngineConfiguration;
 import org.hawkular.agent.monitor.config.AgentCoreEngineConfiguration.MetricsExporterConfiguration;
+import org.hawkular.agent.monitor.config.AgentCoreEngineConfiguration.StorageAdapterConfiguration;
 import org.hawkular.agent.monitor.diagnostics.Diagnostics;
 import org.hawkular.agent.monitor.diagnostics.DiagnosticsImpl;
 import org.hawkular.agent.monitor.diagnostics.JBossLoggingReporter;
@@ -48,12 +49,14 @@ import org.hawkular.agent.monitor.storage.NotificationDispatcher;
 import org.hawkular.agent.monitor.storage.StorageAdapter;
 import org.hawkular.agent.monitor.util.Util;
 import org.hawkular.bus.common.BasicMessage;
+import org.hawkular.inventory.api.model.MetricsEndpoint;
 import org.jboss.logging.Logger;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.ScheduledReporter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -310,7 +313,11 @@ public abstract class AgentCoreEngine {
             protocolServices.start();
 
             // start the metrics exporter if enabled
-            startMetricsExporter();
+            try {
+                startMetricsExporter();
+            } catch (Exception e) {
+                log.errorf(e, "Cannot prepare metrics exporter");
+            }
 
             setStatus(ServiceStatus.RUNNING);
 
@@ -478,6 +485,7 @@ public abstract class AgentCoreEngine {
                 log.infoStartMetricsExporter(args[0], args[1]);
                 metricsExporter = new WebServer();
                 metricsExporter.start(args);
+                sendMetricsEndpointRegistrationRequest();
             } else {
                 log.infoMetricsExporterDisabled();
             }
@@ -599,6 +607,30 @@ public abstract class AgentCoreEngine {
         }
         String status = (String) result.get("status");
         return "UP".equals(status);
+    }
+
+    private void sendMetricsEndpointRegistrationRequest() throws Exception {
+        StorageAdapterConfiguration sac = configuration.getStorageAdapter();
+        MetricsExporterConfiguration mec = configuration.getMetricsExporterConfiguration();
+
+        MetricsEndpoint me = MetricsEndpoint.builder()
+                .feedId(getFeedId())
+                .host(mec.getHost())
+                .port(mec.getPort())
+                .build();
+
+        StringBuilder url = Util.getContextUrlString(sac.getUrl(), sac.getInventoryContext())
+                .append("register-metrics-endpoint");
+        Request request = httpClientBuilder.buildJsonPostRequest(url.toString(), null, Util.toJson(me));
+        Call call = httpClientBuilder.getHttpClient().newCall(request);
+
+        try (Response response = call.execute()) {
+            if (!response.isSuccessful()) {
+                throw new Exception("status-code=[" + response.code() + "], reason=["
+                        + response.message() + "], url=[" + request.url().toString() + "]");
+            }
+        }
+        log.debugf("Metrics endpoint registered");
     }
 
     /**
